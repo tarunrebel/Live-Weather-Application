548618816f02205eebe6947c0e371bec8833dcf7
Tarun sai Sivapuram <tarunsai200@gmail.com>
2021-06-19 08:53:56 +0530

    Added styling

--- weather.js
+++ weather.js
@@ -3,6 +3,7 @@ const https = require("https");
 const app = express();
 const bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.listen(4000, function () {
     console.log("the server has started at port 4000");
@@ -14,32 +15,32 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
     const api = "ccc27e990d647a4e3a0e30752444c914";
-    var cityName = req.body.cityname;
-    const url =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        cityName +
-        "&appid=" +
-        api;
-    https.get(url, function (response) {
-        response.on("data", function (data) {
-            var weatherinfo = JSON.parse(data);
-            var weather = weatherinfo.weather[0].main;
-            var temperature = weatherinfo.main.temp;
-            var place = weatherinfo.name;
-            var icon = weatherinfo.weather[0].icon;
-            var imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write(
-                "<h1>" + "The weather in " + place + " is " + weather + "</h1>"
-            );
-            res.write(
-                "The temperature in " +
-                    place +
-                    " is " +
-                    temperature +
-                    " degree Celcius<br>"
-            );
-            res.write("<img src=" + imgURL + ">");
-            res.send();
-        });
+var cityName = req.body.cityname;
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    api;
+https.get(url, function (response) {
+    response.on("data", function (data) {
+        var weatherinfo = JSON.parse(data);
+        var weather = weatherinfo.weather[0].main;
+        var temperature = weatherinfo.main.temp;
+        var place = weatherinfo.name;
+        var icon = weatherinfo.weather[0].icon;
+        var imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write(
+            "<h1>" + "The weather in " + place + " is " + weather + "</h1>"
+        );
+        res.write(
+            "The temperature in " +
+                place +
+                " is " +
+                temperature +
+                " degree Celcius<br>"
+        );
+        res.write("<img src=" + imgURL + ">");
+        res.send();
     });
+});
 });
